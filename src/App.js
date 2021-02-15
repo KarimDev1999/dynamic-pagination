9a829b8910daf3569a42c921dce5a1844763f9de
Karim <cseed322@gmail.com>
2021-02-15 19:09:52 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+
+
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  let ref = useRef()
+
+  const handleScroll = (e) => {
+    if (e.target.scrollHeight - e.target.scrollTop === e.target.offsetHeight) {
+      setPage(prev => prev + 1)
+    }
+  }
+  useEffect(() => {
+    ref.current.addEventListener('scroll', handleScroll)
+    return () => {
+      ref.current.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setIsLoading(true)
+      let { data } = await axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${page}`)
+      setItems([...items, ...data])
+      setIsLoading(false)
+    }
+    getPhotos()
+  }, [page])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={ref} className='container'>
+      {items.map(item => <div key={item.id} className='data-card'><img src={item.url} alt="img" /></div>)}
+      {isLoading ? <div className='center'>loading...</div> : null}
     </div>
   );
 }
